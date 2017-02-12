616e4282dc53a4f1297e1cadb1a155e5728d3b8f
LuisGLunaB <luis@mkti.mx>
2017-02-11 23:12:40 -0600

    WHERE functions

--- index.php
+++ index.php
@@ -77,12 +77,17 @@ class SQL{
 	public $table = "";
 	public $message = "";
 
-	public $FIELDS = [];
+	public $FIELDS = NULL;
+	public $SELECTFIELDS = NULL;
+	public $IDFIELD = NULL;
+
 	public $datos = array();
 
 	public $QUERY = "";
 	public $SELECT_query = "";
 	public $SELECT_binds = array();
+	public $WHERE_binds = array();
+	public $WHERE_count = 0;
 
 	public $indexFirst = True;
 	public $time = 0;
@@ -91,9 +96,10 @@ class SQL{
 	public $n = 0;
 	public $isEmpty = True;
 
-	public function __construct($con,$table){
+	public function __construct($con,$table,$data=NULL){
 		global $debugging;
 		if(!isset($debugging)){$debugging = False;}
+		if(!is_null($data)){$this->setData($data);}
 
 		$this->debugging = $debugging;
 		$this->con = $con;
@@ -133,15 +139,23 @@ class SQL{
 
 		return $masked;
 	}
-	public static function binding(&$binds,$data,$text = NULL,$init = 0){
+	public static function binding(&$binds,$data,$text = NULL,$posttext="",$init = 0){
 		$associative = is_assoc($data);
 		$nulltext = is_null($text);
 
 		if($associative){
 			$c = $init;
 			foreach($data as $key => $value){
-				  $name = ($nulltext) ? ":$key" : ":$text$c";
-					$binds[$name] = $value;
+				  $name = ($nulltext) ? ":$key$posttext" : ":$text$c";
+					if(!is_array($value)){
+						$binds[$name] = $value;
+					}else{
+						$i = 0;
+						foreach($value as $v){
+							$binds[$name."_$i"] = $v;
+							$i++;
+						}
+					}
 					$c++;
 			}
 		}else{
@@ -154,24 +168,25 @@ class SQL{
 		}
 		return $binds;
 	}
-	public static function implode($binds,$text=", "){
-		$keys = array_keys($binds);
-		return implode($text,$keys);
-	}
 	public function buildQuery(){
 		$query = &$this->QUERY;
 		$query = "";
 
 		$query .= $this->SELECT_query;
+		$query .= $this->WHERE_query;
 		return $query;
 	}
-	public function buildBinder(&$q){
-		$select = &$this->SELECT_binds;
-		self::bindValues($q,$select);
-		return $q;
-	}
 	public function macroBinds(){
-		return array();
+		return $this->WHERE_binds;
+	}
+	public static function implode($binds,$text=", "){
+		$keys = array_keys($binds);
+		return implode($text,$keys);
+	}
+	public function buildBinder(&$q){
+		$where = &$this->WHERE_binds;
+		self::bindValues($q,$where);
+		return $q;
 	}
 	public static function bindValues(&$q,$binds){
 		foreach($binds as $key => $value){
@@ -184,16 +199,28 @@ class SQL{
 		$con = &$this->con;
 		$table = &$this->table;
 		$table_fields = [];
-		try{
-			$q = $con['handler']->prepare("DESCRIBE $table ");
-			$q->execute();
-			$table_fields = $q->fetchAll(PDO::FETCH_COLUMN);
-		} catch(Exception $e){
-			$this->error("* Error en DESCRIBE $table.", $e);
+
+		if(is_null($this->FIELDS)){
+			try{
+				$q = $con['handler']->prepare("DESCRIBE $table ");
+				$q->execute();
+				$table_fields = $q->fetchAll(PDO::FETCH_COLUMN);
+				$this->FIELDS = $table_fields;
+				}catch(Exception $e){
+				$this->error("* Error en DESCRIBE $table.", $e);
+			}
+		}else{
+			$table_fields = $this->FIELDS;
 		}
-		$this->FIELDS = $table_fields;
+
 		return $table_fields;
 	}
+	public function DESCRIBEID(){
+		$fields = $this->DESCRIBE();
+		$this->IDFIELD = $fields[0];
+		return $this->IDFIELD;
+	}
+	
 	public function SELECT($fields = NULL, $mask = NULL){
 		$table = &$this->table;
 		$query = &$this->SELECT_query;
@@ -201,23 +228,89 @@ class SQL{
 		$query = "";
 		if(is_null($fields)){
 			$selection = "*";
-			$this->FIELDS = $this->DESCRIBE();
+			$this->SELECTFIELDS = $this->DESCRIBE();
 		}else{
 			$mask = (is_null($mask)) ? $this->DESCRIBE() : $mask;
 			$fields = self::mask($fields,$mask);
 			$selection = implode(", ",$fields);
-			$this->FIELDS = $fields;
+			$this->SELECTFIELDS = $fields;
 		}
 		$query = "SELECT $selection FROM $table ";
 		return $query;
 	}
+	public function WHERE($where, $mask = NULL, $symbol = "="){
+		$query = &$this->WHERE_query;
+		$binds = &$this->WHERE_binds;
+		$count = &$this->WHERE_count;
+		$binds = ($query=="") ? array() : $binds;
+		$count = ($query=="") ? 0 : $count;
+		$query = ($query=="") ? "WHERE" : $query;
+
+		$validSymbols = ["=","<",">","<=",">=","LIKE","IN","BETWEEN"];
+		$symbol = ( in_array($symbol,$validSymbols) ) ? $symbol : "=";
+		$mask = (is_null($mask)) ? $this->DESCRIBE() : $mask;
+		$where = self::mask($where,$mask);
+
+		if( sizeof($where)>0 ){
+			$posttext = "_where$count";
+			self::binding($binds,$where,NULL,$posttext);
+			$keys = array_keys($where);$keystext = "";
+			$AND = ( $query=="WHERE" ) ? "" : "AND";
+			foreach($keys as $key){
+				$keystext .= "$AND $key $symbol ";
+
+				if(!is_array($where[$key])){
+					$keystext .= ":$key$posttext ";
+				}else{
+					$binded = []; $i = 0;
+					foreach($where[$key] as $v){$binded[] = ":$key$posttext"."_$i"; $i++;}
+					$keystext .= "( " . implode(", ",$binded) . " ) ";
+				}
+
+				$AND = "AND";
+			}
+			$query .= $keystext;
+		}
+		$count++;
+		return $query;
+	}
+	public function BETWEEN($where,$mask=NULL,$inclusive=True){
+		if(is_bool($mask)){$inclusive = $mask;$mask = NULL;}
+		$lower = array(); $upper = array();
+		foreach($where as $key => $value){
+			$lower[$key] = $value[0];
+			$upper[$key] = $value[1];
+		}
+		$symbol = ($inclusive) ? "<=" : "<";
+		$this->WHERE($lower,$mask,">=");
+		return $this->WHERE($upper,$mask,$symbol);
+	}
+	public function GREATER($where,$mask=NULL,$inclusive=True){
+		if(is_bool($mask)){$inclusive = $mask;$mask = NULL;}
+		$symbol = ($inclusive) ? ">=" : ">";
+		return $this->WHERE($where,$mask,$symbol);
+	}
+	public function LOWER($where,$mask=NULL,$inclusive=True){
+		if(is_bool($mask)){$inclusive = $mask;$mask = NULL;}
+		$symbol = ($inclusive) ? "<=" : "<";
+		return $this->WHERE($where,$mask,$symbol);
+	}
+	public function IN($where,$mask=NULL){
+		return $this->WHERE($where,$mask,"IN");
+	}
+	public function WHEREID($id){
+		$id = (int) $id;
+		$field = $this->DESCRIBEID();
+		return $this->WHERE( array( $field => $id), [$field] );
+	}
+
 	public function EXECUTE(&$q = NULL){
 		# Reference $q properly
 		if(is_null($q)){$q = &$this->q;}
 		$con = &$this->con;
 		$datos = &$this->datos;
 		$datos = array();
-		$fields = &$this->FIELDS;
+		$fields = &$this->SELECTFIELDS;
 		$this->status = True;
 		# Query statistics
 		$startT = microtime(true); // Measure time
@@ -225,9 +318,12 @@ class SQL{
 		try{
 			# Execution and handling
 			$this->buildQuery();
+			$macroBinds = $this->macroBinds();
 			$q['handle'] = $con['handler']->prepare($this->QUERY);
 			$q['handle']->setFetchMode(PDO::FETCH_ASSOC);
-			$q['handle']->execute( $this->macroBinds() ); //$this->buildBinder($q);
+			//echo $this->QUERY;print_r($macroBinds);
+			$this->buildBinder($q);
+			$q['handle']->execute();
 			# Turn data into Array
 			$c = 0;
 			while( $row=$q['handle']->fetch() ) {
@@ -253,11 +349,57 @@ class SQL{
 
 		return $datos;
 	}
+	public function setData($data){
+		if(is_array($data)){
+			$this->datos = $data;
+			}else{
+			$this->datos = json_decode($data);
+		}
+	}
+	public function json($show=True){
+		$json = json_encode($this->datos);
+		if($show){echo $json;}
+		return $json;
+	}
+	public function show($show=True){
+		$datos = &$this->datos;
+		$IF = &$this->indexFirst;
+		if($IF){
+			$lines = array_keys($datos);
+			$fields = array_keys($datos[$lines[0]]);
+			}else{
+			$fields = array_keys($datos);
+			$lines = array_keys($datos[$lines[0]]);
+		}
+
+		$table = "";
+		$table .= '<table class="phptable">';
+
+			$table .= '<tr style="background-color: black; color: white;">';
+			foreach($fields as $f){$table .="<td>$f</td>";}
+			$table .= "</tr>";
+
+				foreach($lines as $c){
+					$table .= "<tr>";
+					foreach($fields as $f){
+						$value = ($IF) ? $datos[$c][$f] : $datos[$f][$c];
+						$table .="<td>".$value."</td>";
+					}
+					$table .= "</tr>";
+				}
+
+		$table .= "</table>";
+
+		if($show){echo $table;}
+		return $table;
+	}
 }
 
 $con = SQL_Connection::Qconnect("mkti.mx","ventallanta","grupollancarsa","Grupollancarsa22",$debugging);
 $PRODUCTOS = new SQL($con,"productos");
-$PRODUCTOS->SELECT( ["alto","ancho","rin"] );
-print_r($PRODUCTOS->EXECUTE());
-
+$PRODUCTOS->SELECT( ["id","alto","ancho","rin"] );
+$PRODUCTOS->BETWEEN( array("id" => [100,200]) );
+$PRODUCTOS->IN( array("rin"=>[14,19]) );
+$PRODUCTOS->EXECUTE();
+$PRODUCTOS->show();
 ?>
