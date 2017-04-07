950b2a4fc41ab4942e8022b11b93d3a68212441f
LuisGLunaB <luis@mkti.mx>
2017-04-06 23:11:25 -0500

    Navigate: Counts

--- backend/Module-Functionals/NAVIGATE.php
+++ backend/Module-Functionals/NAVIGATE.php
@@ -1,6 +1,11 @@
 <?php
 
 class NAVIGATE{
+  public static $emptyValues =
+    [""," ","  ","-","--","ALL","All","Todos","TODOS","Todas","TODAS",".","_",NULL,
+    "NONE","None","Ninguno","NINGUNO","Ninguna","NINGUNA",
+    "Ningunos","Ningunas","NINGUNOS","NINGUNAS","Nada","NADA","Sin filtro","Sin filtros",
+    "No filter","No filters","Without filter","Without filters"];
 
   public static function PaginationBar($page,$limit,$total,$options=5){
     list($start_page, $current_page, $end_page, $last_page) = self::PaginationVariables($page,$limit,$total,$options);
@@ -28,47 +33,114 @@ class NAVIGATE{
   public static function thousands_format($x){
     return number_format( $x, 0 );
   }
-  public static function money_format($x){
-    return "$".number_format( $x, 2, ".", ",");
+  public static function money_format($x,$symbol="$"){
+    return $symbol.number_format( $x, 2, ".", ",");
   }
 
-  public static function buildFormSelect($Table,$name="",$selected_value=NULL){
+  public static function getCategoriesCount($ViewName,$id=1,$WHERE_query="",$WHERE_binds = []){
+    return NULL;
+  }
+  public static function getFieldCount($ViewName,$id_field,$field,$WHERE_query="",$WHERE_binds = []){
+    $COUNT = new SQLBasicSelector($ViewName);
+    if( $COUNT->isValidTable() and $COUNT->isValidField($field) and $COUNT->isValidField($id_field)){
+      $field_name = ($id_field==$field) ? ($field."_name") : $field;
+      $data = $COUNT->free("
+        SELECT
+          $id_field,
+          $field AS $field_name,
+          COUNT($field) AS ".$field."_count
+        FROM $ViewName
+        $WHERE_query
+        GROUP BY $id_field
+        ORDER BY ".$field."_count DESC
+      " , $WHERE_binds );
+      if ( ! $COUNT->status() ){
+        $data = NULL;
+      }
+    }else{
+      $data = NULL;
+    }
+
+    return $data;
+  }
+
+  public static function buildFormSelect($Table,$selected_value=NULL,$default="--"){
     $keys = array_keys($Table[0]);
+    $name = "name=$keys[0]";
     $value_field = $keys[0];
     $text_field = $keys[1];
+    $count_field = ( sizeof($keys) > 2) ? $keys[2] : NULL;
 
-    $name = ($name=="") ? "" : "name='$name'";
+    $has_selected = False;
+    $selected = "";
+    $select = "";
 
-    $select = "<select $name >";
-      foreach( $Table as $row ){
-        $value = $row[$value_field];
-        $text = $row[$text_field];
-        $selected = ( is_null($selected_value) or ($selected_value != $value) )
-          ? "" : "selected='selected'";
-        $select .= "<option $selected value='$value'>$text</option>";
+    foreach( $Table as $row ){
+      $value = $row[$value_field];
+      $text = $row[$text_field];
+      $count = ( is_null($count_field) ) ? "" : " ($row[$count_field])";
+      if( self::isEmptyValue($value) ){ continue; }
+
+      if( is_null($selected_value) or ($selected_value != $value) ){
+        $selected = "";
+      }else{
+        $selected = "selected='selected'";
+        $has_selected = True;
       }
-    $select .= "</select>";
 
-    return $select;
-  }
-
-  public static function buildDistinctSelect($TableName,$field,$selected_value=NULL,$ORD="ASC"){
-    $SELECT = new SQLBasicSelector($TableName);
-    return $SELECT->free("SELECT DISTINCT $field FROM $TableName ORDER BY $field $ORD");
-  }
-
-  public static function getProductCategories($id){
-    $id = (int) $id;
-    $Category = new SQLBasicSelector("product_categories_$id");
-    $Category->execute();
-    if( ! $Category->status() ){
-      $Category->data = NULL;
+      $select .= "<option $selected value='$value'>$text$count</option>";
     }
-    return $Category->data;
+
+    if( ! $has_selected ){
+      $select = "<option selected value=''>$default</option>".$select;
+    }
+
+    return "<select $name >$select</select>";
   }
-  public static function buildCategorySelect($id,$selected_value=NULL){
+
+  public static function buildDistinctSelect($TableName,$field,$selected_value=NULL,$default="--",$ASC=True){
+    $Table = self::getDistinctOptions($TableName,$field,$ASC);
+    return self::buildFormSelect($Table,$selected_value,$default);
+  }
+  public static function buildFieldSelect($TableName,$field,$selected_value=NULL,$default="--",$ASC=True){
+    $Table = self::getFieldOptions($TableName,$field,$ASC);
+    return self::buildFormSelect($Table,$selected_value,$default);
+  }
+  public static function buildCategorySelect($id,$selected_value=NULL,$default="--",$ASC=True){
     $id = (int) $id;
-    $Table = self::getProductCategories($id);
-    return self::buildFormSelect($Table,"category_".$id."_id",$selected_value);
+    $Table = self::getCategories($id,$ASC);
+    return self::buildFormSelect($Table,$selected_value,$default);
   }
+
+  public static function getDistinctOptions($TableName,$field,$ASC=True){
+    $ASC = ($ASC==True) ? "ASC" : "DESC";
+    $SELECT = new SQLBasicSelector($TableName);
+    return $SELECT->free("SELECT DISTINCT $field, $field AS $field"."_name"." FROM $TableName ORDER BY $field $ASC");
+  }
+  public static function getFieldOptions($Tablename,$field,$ASC=True){
+    $ASC = ($ASC==True) ? "ASC" : "DESC";
+    $SELECT = new SQLBasicSelector($Tablename);
+    if( $SELECT->isValidTable() and $SELECT->isValidField($field) ){
+      $id_field = $SELECT->getTableId();
+      $SELECT->free("SELECT $id_field, $field FROM $Tablename ORDER BY $field $ASC ");
+      if( ! $SELECT->status() ){
+        $SELECT->data = NULL;
+      }
+    }else{
+      $SELECT->data = NULL;
+    }
+    return $SELECT->data;
+  }
+  public static function getCategories($id,$ASC=True){
+    $id = (int) $id;
+    return self::getFieldOptions("product_categories_$id","category_".$id."_name",$ASC);
+  }
+
+  public static function hasValue($value){
+    return ( ! self::isEmptyValue($value) );
+  }
+  public static function isEmptyValue($value){
+    return in_array($value,self::$emptyValues);
+  }
+
 }
--- products.php
+++ products.php
@@ -3,14 +3,25 @@
   include_once( ROOT . "/backend/Loaders/LOADMODULE_SQL_BASIC.php");
   /* Enviroment: $SQLConnection, $con */
   if( $SQLConnection->status() ){
-    //$categories_1 = NAVIGATE::getProductCategories(1);
-    //$categories_2 = NAVIGATE::getProductCategories(2);
-
-
     $PRODUCTS = new SQLBasicSelector("products_view_ledcity");
     $PRODUCTS->PAGE(0,20);
     $TOTAL = $PRODUCTS->TOTAL();
     $PRODUCTS->execute();
+
+    /*
+    TODO:
+      - Make getCategoriesCount call getFieldsCount
+      - Refactor NAVIGATE
+      - Parse POST:
+        1. Fill with NULLs given expected keys ( is_null($_POST[x]) )
+        2. Ignore emptyValues mask ( isset($_POST[x]) )
+    */
+
+    $distinct_count = NAVIGATE::getFieldCount("products_view_ledcity","voltage","voltage");
+    echo NAVIGATE::buildFormSelect($distinct_count);
+
+    $distinct_count = NAVIGATE::getFieldCount("products_view_ledcity","category_1_id","category_1_name");
+    echo NAVIGATE::buildFormSelect($distinct_count);
 
   }
 
@@ -26,6 +37,7 @@
     <?php
     echo NAVIGATE::buildCategorySelect(1);
     echo NAVIGATE::buildCategorySelect(2);
+
     echo NAVIGATE::PaginationBar(0,20,$TOTAL);
     DISPLAY::asTable( $PRODUCTS->data );
     ?>
